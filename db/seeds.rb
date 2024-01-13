5a150670200ef491a06b88a19456b16955e7641b
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-13 15:14:55 +0100

    gem issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  title = Faker::Book.title
+  text = Faker::Lorem.words(number: 50)
+  article = Article.new(title: title, text: text)
+  article.save
+end
